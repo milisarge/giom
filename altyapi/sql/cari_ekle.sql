3f46f84d381fc57190ba29f4a48e9ed0a196b5e3
milisbir <milisarge@gmail.com>
2018-01-20 12:51:47 +0200

    cari_ekle

--- altyapi/sql/cari_ekle.sql
+++ altyapi/sql/cari_ekle.sql
@@ -1,5 +1,5 @@
 insert into tblcasabit 
-(cari_kod,cari_isim,cari_tel,cari_tel2,grup_kodu,cari_tip,
+(SUBE_KODU,ISLETME_KODU,cari_kod,cari_isim,cari_tel,cari_tel2,grup_kodu,cari_tip,
 vergi_dairesi,vergi_numarasi,email,vade_gunu,cari_adres,iban,rapor_kodu1)
-values('@kod@','@isim@','@tel@','@tel2@','@grupkod@',
+values(0,0,'@kod@','@isim@','@tel@','@tel2@','@grupkod@',
 '@cari_tip@','@vergiyer@','@vergino@','@eposta@','@vade_gunu@','@adres@','@iban@','@kod1@');
